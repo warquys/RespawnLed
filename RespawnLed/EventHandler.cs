5b843788b2545fcfc458e0d50ebeb9e80df35ea9
Valentin Arthur Thomas <warquis958@gmail.com>
2023-05-08 03:01:49 +0200

    Ajoutez des fichiers projet.

---
+++ RespawnLed/EventHandler.cs
@@ -0,0 +1,40 @@
+﻿using Neuron.Core.Events;
+using Respawning;
+
+namespace RespawnLed;
+
+[Automatic]
+public class EventHandler : Listener
+{
+    private readonly RoomService _room;
+
+    public EventHandler(RoomService room)
+    {
+        _room = room;
+    }
+
+    [EventHandler]
+    public void OnSpawn(SpawnTeamEvent ev)
+    {
+        switch (ev.TeamId)
+        {
+            case (uint)SpawnableTeamType.NineTailedFox:
+                ChangeFacilityColor(Color.blue);
+                break;
+            case (uint)SpawnableTeamType.ChaosInsurgency:
+                ChangeFacilityColor(Color.green);
+                break;
+            default:
+                ChangeFacilityColor(default);
+                break;
+        }
+    }
+
+    private void ChangeFacilityColor(Color color)
+    {
+        foreach (var room in _room.Rooms)
+        {
+            room.RoomColor = color;
+        }
+    }
+}
---
+++ RespawnLed/RespawnLed.cs
@@ -0,0 +1,14 @@
+﻿namespace RespawnLed;
+
+
+[Plugin(
+    Name = "AirBomb",
+    Description = "Change the colors of all room when a new team spawn",
+    Version = "1.0.0",
+    Author = "VT",
+    Repository = "https://github.com/warquys/RespawnLed"
+    )]
+public class RespawnLed : Plugin
+{
+
+}
